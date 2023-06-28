cb148ee0a832d8ad8f2fbf1ca8c09e3c5d02417d
JESin10 <shinej1029@gmail.com>
2023-06-28 18:06:02 +0900

    Basic TodoList setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+interface Todo {
+  id: number;
+  // title: string;
+  content: string;
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputText, setInputText] = useState("");
+  const idRef = useRef(0);
+
+  const onChangeTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const onClickAddTodo = () => {
+    setTodos([...todos, { id: idRef.current++, content: inputText }]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <input value={inputText} onChange={onChangeTodoInput} />
+      <button onClick={onClickAddTodo}> add</button>
+    </div>
+  );
 }
 
 export default App;
